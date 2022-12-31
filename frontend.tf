a55c6a414e7f78a34c0792c6d91fe959bdd84209
Miskeen <hussainajhar8@gmail.com>
2022-12-31 18:24:41 +0000

    added gitlab pipeline and config files

---
+++ frontend.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_deployment" "ft-deploy" {
+  metadata {
+    name = "ft-deploy"
+    labels = {
+      name = "ft-deploy"
+    }
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        name = "web1"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          name = "webapp"
+        }
+      }
+      spec {
+        container {
+          image = "nginx"
+          name  = "webapp-1"
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
